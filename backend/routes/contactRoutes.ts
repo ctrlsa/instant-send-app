d70854ab6e78cc9bef6cc5ac864bcd550b6e2165
Dasarath G <dasarathg68@gmail.com>
2024-10-23 21:10:50 +0530

    temp: remove middleware

--- backend/routes/contactRoutes.ts
+++ backend/routes/contactRoutes.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { deleteContact, getContacts } from "../controllers/contactController";
-import { authorizeUser } from "../middleware/authMiddleware";
+// import { authorizeUser } from "../middleware/authMiddleware";
 
 const contactRoutes = express.Router();
 
 contactRoutes.get("/", (req, res) => {
   res.send("Hello from the contact routes!");
 });
-contactRoutes.get("/getContacts/:id", authorizeUser, getContacts);
-contactRoutes.delete("/deleteContact/:id", authorizeUser, deleteContact);
+contactRoutes.get("/getContacts/:id", getContacts);
+contactRoutes.delete("/deleteContact/:id", deleteContact);
 
 export default contactRoutes;
